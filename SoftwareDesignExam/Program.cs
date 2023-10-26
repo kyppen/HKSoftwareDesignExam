f4460569ec9ac45cd2d33be12c51a7d71fc93598
fam <fagerhus.sondre@gmail.com>
2023-10-26 14:57:59 +0200

    something

--- SoftwareDesignExam/Program.cs
+++ SoftwareDesignExam/Program.cs
@@ -2,6 +2,8 @@
 class Program{
     static public void Main(String[] args)
     {
+        
+        ///
         Console.WriteLine("Starting program!");
         Item item = new("0", "bread", 6.1);
         Console.WriteLine(item.ToString());

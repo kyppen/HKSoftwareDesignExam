3e9b7e7fa616834b55dfbc7be3d20787db181210
fam <fagerhus.sondre@gmail.com>
2023-11-02 18:00:01 +0100

    added Login and email/password validation

--- SoftwareDesignExam/Menu/MenuPrintOptions.cs
+++ SoftwareDesignExam/Menu/MenuPrintOptions.cs
@@ -1,5 +1,6 @@
 namespace SoftwareDesignExam.Menu;
 
+
 public class MenuPrintOptions{
     public void GuestMainMenu()
     {
@@ -20,5 +21,30 @@ public class MenuPrintOptions{
                           "7: View cart\n" +
                           "8: Log out");
     }
+
+    public static void CreateUser()
+    {
+        string[] passwords = {"", ""};
+        Console.WriteLine("User Form:");
+        
+        Console.WriteLine("Enter your Email address:\n");
+        string email = Console.ReadLine();
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine("Enter your password");
+        passwords[0] = Console.ReadLine();
+        Console.WriteLine("Re-enter your password");
+        passwords[1] = Console.ReadLine();
+        
+        if(MenuUtils.ValidateEmail(email) && MenuUtils.ValidatePassword(passwords))
+        {
+            Console.WriteLine("User has been created");
+            //Function to create user and add it to datebase
+        }
+        else
+        {
+            Console.WriteLine("There was an error creating user, Either the passwords didnt match or you had more then one @ in your email");
+        }
+    }
+    
     
 }

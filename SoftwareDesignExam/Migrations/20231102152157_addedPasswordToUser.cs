8f118900aeed4cc40659f3fbafa4a45a12cf7603
poteter <eith2312@gmail.com>
2023-11-02 16:23:02 +0100

    updated migrations

---
+++ SoftwareDesignExam/Migrations/20231102152157_addedPasswordToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareDesignExam.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedPasswordToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "User_Password",
+                table: "User",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User_Password",
+                table: "User");
+        }
+    }
+}

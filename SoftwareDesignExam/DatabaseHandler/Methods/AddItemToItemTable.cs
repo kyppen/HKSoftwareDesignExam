72be5f8b2bfb2c5c1163a12226d85aa971d42f56
poteter <eith2312@gmail.com>
2023-11-02 15:40:34 +0100

    added databaseHandler/methods/ addItemToItemTable and ReadAllItemsFromItemTable

---
+++ SoftwareDesignExam/DatabaseHandler/Methods/AddItemToItemTable.cs
@@ -0,0 +1,24 @@
+﻿using SoftwareDesignExam.DataAccess.SqLite;
+using SoftwareDesignExam.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignExam.DatabaseHandler.Methods {
+	public class AddItemToItemTable {
+		public static int Add(string name, string description, int price) {
+			Item item = new Item() {
+				Item_Name = name,
+				Item_Description = description,
+				Item_Price = price
+			};
+
+			using StoreDbContext db = new StoreDbContext();
+			db.Item.Add(item);
+			db.SaveChanges();
+			return item.Id;
+		}
+	}
+}
--- SoftwareDesignExam/Program.cs
+++ SoftwareDesignExam/Program.cs
@@ -1,44 +1,18 @@
 ﻿using SoftwareDesignExam.Items;
 using SoftwareDesignExam.UserManagement;
 using SoftwareDesignExam.ShoppingList;
+using SoftwareDesignExam.DatabaseHandler.Methods;
 
 namespace SoftwareDesignExam;
 class Program 
 {
     static public void Main(String[] args)
     {
-        Console.WriteLine("Starting program!");
+        AddItemToItemTable.Add("Jarlsberg", "Yellow Cheese", 99);
+        AddItemToItemTable.Add("Toro Tomatsuppe", "Toro Tomatosoup in bag", 23);
+        AddItemToItemTable.Add("Grandiosa", "Frozen classic Pizza Grandiosa", 45);
 
-        var user = new User("123abc", "Alex", "alex@example.com");
-
-        var factory = new ShoppingListFactory();
-
-        var regularshoppingList = factory.CreateList("Regular", "list001", $"{user.Username}-RegularList");
-
-        var holidayshoppingList = factory.CreateList("Holiday", "list002", $"{user.Username}-HolidayList");
-
-        var item1 = new Item("001", "Apple", 0.50);
-        var item2 = new Item("002", "Orange", 0.60);
-
-        regularshoppingList.AddItem(item1);
-        regularshoppingList.AddItem(item2);
-
-        holidayshoppingList.AddItem(item1);
-        holidayshoppingList.AddItem(item2);
-
-        Console.WriteLine("Before delte item");
-        DisplayItems(regularshoppingList);
-        Console.WriteLine($"Total price for Regular Shopping List before delete: ${Math.Round(regularshoppingList.GetTotalPrice(), 2):0.00}\n");
-
-        DeleteItem(regularshoppingList, item1);
-
-        Console.WriteLine("After delete item");
-        // Math.Round method to round the total price to 2 decimal places
-        DisplayItems(regularshoppingList);
-        Console.WriteLine($"Total price for Regular Shopping List: ${Math.Round(regularshoppingList.GetTotalPrice(), 2):0.00}\n");
-
-        DisplayItems(holidayshoppingList);
-        Console.WriteLine($"Total price for Holiday Shopping List: ${Math.Round(holidayshoppingList.GetTotalPrice(), 2):0.00}");
+        ReadAllItemsFromItemTable.Read();
     }
 
     private static void DisplayItems(AbstractShoppingList shoppingList)

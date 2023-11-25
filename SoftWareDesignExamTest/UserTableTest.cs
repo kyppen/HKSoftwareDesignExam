4878eecbf03899c4aa99b6ea29c75f455e277b91
poteter <eith2312@gmail.com>
2023-11-25 18:03:39 +0100

    minor fixes and test updates

--- SoftWareDesignExamTest/UserTableTest.cs
+++ SoftWareDesignExamTest/UserTableTest.cs
@@ -1,5 +1,5 @@
 using SoftwareDesignExam.Controller;
-using SoftwareDesignExam.DatabaseHandler.Methods.UserTableMethods;
+using SoftwareDesignExam.DataAccess;
 using SoftwareDesignExam.Entities;
 
 namespace SoftWareDesignExamTest;
@@ -14,30 +14,36 @@ public class UserTableTest
     [SetUp]
     public void Setup()
     {
-        _id = UserController.CreateUser("Lady", "Gaga", "ladygaga@gmail.com", "password");
+		SqLiteUserDataAccess sqlUser = new SqLiteUserDataAccess();
+		UserController userController = new UserController(sqlUser);
+		_id = userController.CreateUser("Lady", "Gaga", "ladygaga@gmail.com", "password");
 
     }
 
     [Test]
     public void Test_UserIntegrity()
     {
-        List<User> answer = ReadUserFromUserTable.Read("ladygaga@gmail.com", "password");
+		SqLiteUserDataAccess sqlUser = new SqLiteUserDataAccess();
+		UserController userController = new UserController(sqlUser);
+		List<User> answer = userController.Read("ladygaga@gmail.com", "password");
         Assert.That(answer[0].User_LName, Is.EqualTo("Gaga"));
         Assert.That(answer[0].User_FName, Is.EqualTo("Lady"));
         Assert.That(answer[0].User_Email, Is.EqualTo("ladygaga@gmail.com"));
         Assert.That(answer[0].User_Password, Is.EqualTo("password"));
-        RemoveUserFromUserTable.Remove(_id);
+        userController.Remove(_id);
 
     }
 
     [Test]
     public void Test_RemoveUserFromUserTable()
     {
-        // Remove the user from _id
-        RemoveUserFromUserTable.Remove(_id);
+		SqLiteUserDataAccess sqlUser = new SqLiteUserDataAccess();
+		UserController userController = new UserController(sqlUser);
+		// Remove the user from _id
+		userController.Remove(_id);
 
         // Try to read the removed user from the table
-        var userAfterRemove = ReadUserFromUserTable.Read("ladygaga@gmail.com", "password");
+        var userAfterRemove = userController.Read("ladygaga@gmail.com", "password");
         Console.WriteLine(userAfterRemove.Count);
 
         // Assert that the user is no longer in the table (expecting an empty list)
@@ -47,25 +53,21 @@ public class UserTableTest
     [Test]
     public void Test_CheckForDuplicationEmail()
     {
-        // Check email exist before
-        bool beforeAdd = CheckForDuplicateEmail.Check(_email);
+		SqLiteUserDataAccess sqlUser = new SqLiteUserDataAccess();
+		UserController userController = new UserController(sqlUser);
+		// Check email exist before
+		bool beforeAdd = userController.CheckDuplicate(_email);
 
         // Email should not exist before addiing a new user
         Assert.IsFalse(beforeAdd);
 
 
         // Check email exist after
-        bool afterAdd = CheckForDuplicateEmail.Check(_email2);
+        bool afterAdd = userController.CheckDuplicate(_email2);
 
         // Email should exist after adding a new user
         Assert.IsTrue(afterAdd);
-        RemoveUserFromUserTable.Remove(_id);
+		userController.Remove(_id);
 
-    }
-
-    [Test]
-    public void Test_duplicateItemsInShoppingList()
-    {
-        
     }
 }

e934167c3e3f79aa59f90a62e14db873ca5c8faa
poteter <eith2312@gmail.com>
2023-11-27 13:48:28 +0100

    adding logging

--- SoftwareDesignExam/DatabaseHandler/PopulateDataBase/PopulateUserTable.cs
+++ SoftwareDesignExam/DatabaseHandler/PopulateDataBase/PopulateUserTable.cs
@@ -1,5 +1,6 @@
 ﻿using SoftwareDesignExam.Controller;
 using SoftwareDesignExam.DataAccess.SqLite;
+using SoftwareDesignExam.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,12 @@ using System.Threading.Tasks;
 namespace SoftwareDesignExam.DatabaseHandler.PopulateDataBase {
 	public class PopulateUserTable {
 		public void Populate(UserController userController) {
+			Logger.Instance.LogInformation("[  Starting populating User table with 4 users  ]");
 			userController.CreateUser("King", "Harkinian", "harkinian@hyrule.official.co.uk.ru", "123Shipsflakes%");
 			userController.CreateUser("Roald", "Troll", "troll@email.com", "ofdshfi74");
 			userController.CreateUser("Joe", "Biden", "potus@whitehouse.com", "what11111111111");
 			userController.CreateUser("Snåsa", "Mannen", "snaasamannen@yandex.com", "varmehender123");
-		} 
+			Logger.Instance.LogInformation("[  Finished populating User table  ]");
+		}
 	}
 }
--- SoftwareDesignExam/Program.cs
+++ SoftwareDesignExam/Program.cs
@@ -1,26 +1,14 @@
-﻿using SoftwareDesignExam.Controller;
-using SoftwareDesignExam.Items;
-using SoftwareDesignExam.UserManagement;
-using SoftwareDesignExam.ShoppingList;
-using SoftwareDesignExam.UserManagement;
-using SoftwareDesignExam.Menu;
-using SoftwareDesignExam.DatabaseHandler.PopulateDataBase;
-using SoftwareDesignExam.Controller;
-using SoftwareDesignExam.UIColorController;
+﻿using SoftwareDesignExam.Menu;
 using SoftwareDesignExam.Store;
-using Microsoft.Extensions.DependencyInjection;
-using SoftwareDesignExam.DataAccess;
-using Microsoft.Extensions.Logging;
 
 namespace SoftwareDesignExam;
 class Program
 {
     static public void Main(String[] args)
     {
-		StoreController storeController = new();
-
 		MainMenu.startMenu();
 
+		StoreController storeController = new();
 		TestRunner testRunner = new();
 		testRunner.Run(storeController);
 	}
